efb1fb99bd75bd7d72b7feddd7493f093f53e051
Muhammad <umedjon02.ud@gmail.com>
2023-02-28 21:16:25 +0500

    Correct linters in js files

--- src/index.js
+++ src/index.js
@@ -11,121 +11,120 @@ const list = document.querySelector('.my-list');
 
 // function populateTask() {
 const title = document.createElement('div');
-  title.innerHTML = `<h3 class="title">Today's To Do <i class='fas fa-sync'></i></h3>`;
-  title.classList.add('border-bottom');
-  list.appendChild(title);
+title.innerHTML = '<h3 class="title">Today\'s To Do <i class=\'fas fa-sync\'></i></h3>';
+title.classList.add('border-bottom');
+list.appendChild(title);
 
-  const addInput = document.createElement('div');
-  addInput.innerHTML = `<form action="" id="add-form">
+const addInput = document.createElement('div');
+addInput.innerHTML = `<form action="" id="add-form">
   <input class="add-input" placeholder="Add to your list..."></input>
   <button class="add-btn"><i class="fas fa-level-down-alt"></i></button>
   </form>`;
 
-  addInput.classList.add('border-bottom');
-  list.appendChild(addInput);
+addInput.classList.add('border-bottom');
+list.appendChild(addInput);
 
-  const listUl = document.createElement('table');
-  list.appendChild(listUl);
+const listUl = document.createElement('table');
+list.appendChild(listUl);
 
-  const clearAllDone = document.createElement('div');
-  clearAllDone.innerHTML = '<button class="delete-btn">Clear all completed</button>';
-  list.appendChild(clearAllDone);
+const clearAllDone = document.createElement('div');
+clearAllDone.innerHTML = '<button class="delete-btn">Clear all completed</button>';
+list.appendChild(clearAllDone);
 // }
 
 function render() {
-   const listUl = document.querySelector('table');
-   listUl.innerHTML = '';
+  const listUl = document.querySelector('table');
+  listUl.innerHTML = '';
   arrList.forEach((task) => {
-      const listItem = document.createElement('li');
-       listItem.innerHTML = `
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
       <div class="${task.completed ? 'line-through' : ''}">
-        <input class="check" ${task.completed ? 'checked':''} type="checkbox" id="${task.index}"></input>
+        <input class="check" ${task.completed ? 'checked' : ''} type="checkbox" id="${task.index}"></input>
         <label class ="text">${task.description}</label>
         <input class="editInput hide" type="text" placeholder="Edit your task"></input>
       </div>
-      <button class="elipse-btn ${task.completed ? 'hide':''}" id="${task.index}"><i class="fa fa-ellipsis-v"></i></button>
-      <button class="trash-btn ${task.completed ? '':'hide'}"><i class="fas fa-trash"></i></button>`;
+      <button class="elipse-btn ${task.completed ? 'hide' : ''}" id="${task.index}"><i class="fa fa-ellipsis-v"></i></button>
+      <button class="trash-btn ${task.completed ? '' : 'hide'}"><i class="fas fa-trash"></i></button>`;
     listItem.classList.add('border-bottom');
     listItem.classList.add('list-item');
     listUl.appendChild(listItem);
   });
 }
- render()
+render();
 
-function save () {
+function save() {
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(arrList));
 }
 
-listUl.addEventListener('click', deleteCheck);
+// listUl.addEventListener('click', deleteCheck);
 function deleteCheck(ev) {
   const elipseBtn = document.querySelectorAll('.elipse-btn');
-const trashBtn = document.querySelectorAll('.trash-btn');
+  const trashBtn = document.querySelectorAll('.trash-btn');
   const item = ev.target;
-  if(item.classList[0] === 'check') {
+  if (item.classList[0] === 'check') {
     const index = item.id;
     const parent = item.parentElement;
-    parent.classList.toggle('line-through'); 
+    parent.classList.toggle('line-through');
 
     elipseBtn[index].classList.toggle('hide');
     trashBtn[index].classList.toggle('hide');
-    arrList[index].completed = !arrList[index].completed ;
+    arrList[index].completed = !arrList[index].completed;
     save();
   }
-  if(item.classList[0] === 'trash-btn'){
+  if (item.classList[0] === 'trash-btn') {
     const index = item.previousElementSibling.id;
     const parent = item.parentElement;
     parent.remove();
     arrList.splice(index, 1);
     arrList.forEach((object, index) => {
       object.index = index;
-    })
-    save()
-    render()
+    });
+    save();
+    render();
   }
 }
 
-const editInput = document.querySelectorAll('.editInput')
+listUl.addEventListener('click', deleteCheck);
+
+const editInput = document.querySelectorAll('.editInput');
 const text = document.querySelectorAll('.text');
-text.forEach((label, index) => label.addEventListener('click', () => edit(label, index)));
+// text.forEach((label, index) => label.addEventListener('click', () => edit(label, index)));
 
 function edit(label, index) {
   label.classList.add('hide');
   editInput[index].classList.remove('hide');
   editInput[index].focus();
   editInput[index].addEventListener('keyup', (event) => {
-    if(event.key === 'Enter' && editInput[index].value !='') {
+    if (event.key === 'Enter' && editInput[index].value !== '') {
       label.textContent = editInput[index].value;
       editInput[index].classList.add('hide');
       label.classList.remove('hide');
       arrList[index].description = editInput[index].value;
-      save()
-    } 
+      save();
+    }
   });
-  
-  editInput[index].addEventListener('focusout', (event) => {
-console.log('you are clicking outside of the input field');
-editInput[index].classList.add('hide');
-label.classList.remove('hide');
 
-  })
+  editInput[index].addEventListener('focusout', () => {
+    editInput[index].classList.add('hide');
+    label.classList.remove('hide');
+  });
 }
+
+text.forEach((label, index) => label.addEventListener('click', () => edit(label, index)));
 
 // clear tasks that are completed
 
 const deleteBtn = document.querySelector('.delete-btn');
 
 deleteBtn.addEventListener('click', () => {
-  const listsDone = arrList.filter(item => {
-  return item.completed === false;
-});
+  const listsDone = arrList.filter((item) => item.completed === false);
   arrList = listsDone;
   arrList.forEach((object, index) => {
     object.index = index;
-  })
-  save()
-  render()
+  });
+  save();
+  render();
   window.location.reload();
-})
+});
 
-listJS(arrList, render, save)
-
+listJS(arrList, render, save);
--- src/list.js
+++ src/list.js
@@ -1,16 +1,14 @@
-  
-export default function listJS (arrList, render, save) {
-const input = document.querySelector('.add-input');
-const addForm = document.querySelector('#add-form');
+export default function listJS(arrList, render, save) {
+  const input = document.querySelector('.add-input');
+  const addForm = document.querySelector('#add-form');
 
-addForm.addEventListener('submit', e => {
+  addForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const task = input.value;
-    if((task === null) || (task === '')) return;
-    arrList.push({description:task, completed: false, index:arrList.length});
+    if ((task === null) || (task === '')) return;
+    arrList.push({ description: task, completed: false, index: arrList.length });
     input.value = null;
     render();
     save();
-})
-
+  });
 }
