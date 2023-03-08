07638228655802a00024ec7f597e9f7cec0dd3ba
Muhammad <umedjon02.ud@gmail.com>
2023-03-09 02:30:09 +0500

    Test remove

--- addDelete.test.js
+++ addDelete.test.js
@@ -2,10 +2,13 @@
  * @jest-environment jsdom
  */
 
-import testAddTask from './listTest.js';
+import { testAddTask, testDeleteTask } from './listTest.js';
 
 describe('Add and Remove <li> inside <ul>', () => {
   test('Add <li>', () => {
     expect(testAddTask('Buy Ferrary')).toBe('<ul><li>Buy Ferrary</li></ul>');
+  });
+  test('Remove <li>', () => {
+    expect(testDeleteTask(0)).toBe('<ul></ul>');
   });
 });
--- listTest.js
+++ listTest.js
@@ -1,17 +1,26 @@
+const arrList = [];
+let DOMMOCK = '';
 const testAddTask = (task) => {
-  const arrList = [];
-  if (task === null || task === '') return;
+  if (task === null || task === '') return DOMMOCK;
   arrList.push({
     description: task,
     completed: false,
     index: arrList.length,
   });
 
-  const DOMMOCK = `<ul>${arrList.map(
-    (item) => `<li>${item.description}</li>`
+  DOMMOCK = `<ul>${arrList.map(
+    (item) => `<li>${item.description}</li>`,
   )}</ul>`;
   return DOMMOCK;
 };
 
-testAddTask('Buy Ferrary');
-export default testAddTask;
+const testDeleteTask = (index) => {
+  arrList.splice(index, 1);
+
+  DOMMOCK = `<ul>${arrList.map(
+    (item) => `<li>${item.description}</li>`,
+  )}</ul>`;
+  return DOMMOCK;
+};
+
+export { testAddTask, testDeleteTask };
